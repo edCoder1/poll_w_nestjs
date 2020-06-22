357730600b790ee84a1d000c65c26d44247338fd
edCoder1 <edwin.lsb1@gmail.com>
2020-06-21 19:32:57 -0500

    basic redis connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as AsyncRedis from 'async-redis';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log('Server running...');
 }
+
+async function createClientAndConnectToRedis() {
+  const asyncClient = await AsyncRedis.createClient({
+    port: 6379,
+    // more settings here
+  });
+
+  console.log(asyncClient.server_info);
+
+  asyncClient.on('error', e => {
+    console.log('Error while connecting to redis...');
+    console.log(e);
+  });
+  asyncClient.on('connect', () => {
+    console.log('Connected to redis...');
+  });
+}
+
 bootstrap();
+createClientAndConnectToRedis();
--- src/poll/entry/entry.controller.ts
+++ src/poll/entry/entry.controller.ts
@@ -21,4 +21,7 @@ export class EntryController {
       message: `All Correct: ${id}`,
     };
   }
+
+  @Get('test')
+  public testRedis() {}
 }
